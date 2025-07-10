6942815a561bd5499e11ab7f649eebc46f192135
Noah LaLonde <33038393+JustAnotherIdea@users.noreply.github.com>
2025-07-09 22:03:55 -0500

    version bump

--- auto_wall.py
+++ auto_wall.py
@@ -8,7 +8,7 @@ import atexit
 from src.utils.update_checker import check_for_updates, fetch_version
 
 # Version information - will be updated by GitHub workflow
-APP_VERSION = "0.9.9"
+APP_VERSION = "1.2.0"
 GITHUB_REPO = "ThreeHats/auto-wall"
 
 # Add the project root directory to the Python path
